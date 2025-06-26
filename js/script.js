f7f0c31e4aaf963487a2c5e6f04e478050e50dd3
Florian706 <messangaflorian00@gmail.com>
2025-06-26 14:43:57 -0700

    Ajout des interactions JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Affiche un message dans la console au chargement
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Bienvenue sur le site de l’Entreprise MESSANGA !");
+  });
+  
+  // Exemple d’interaction simple
+  window.addEventListener('scroll', () => {
+    console.log("Défilement de la page détecté !");
+  });
